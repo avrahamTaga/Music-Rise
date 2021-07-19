8ef4a047368369e87afff4d6bac5a1ced9621a3e
Avraham Taga <tagaavraham12@gmail.com>
2021-07-19 15:37:26 +0300

    First Commit

---
+++ controllers/file.js
@@ -0,0 +1,6 @@
+const auth = require("./auth");
+const File = require("../models/file");
+const User = require("../models/user");
+const multer = require("multer");
+
+module.exports.uplaod = (req, res) => {};
